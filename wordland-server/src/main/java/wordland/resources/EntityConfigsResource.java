46a1b61e7392ab89c524c43d16ded59a039536a7
Jonathan Cobb <jonathan@kyuss.org>
2018-09-02 02:05:23 -0400

    add list models, fix test path

--- wordland-server/src/main/java/wordland/resources/EntityConfigsResource.java
+++ wordland-server/src/main/java/wordland/resources/EntityConfigsResource.java
@@ -6,20 +6,25 @@ import lombok.extern.slf4j.Slf4j;
 import org.cobbzilla.wizard.model.entityconfig.ModelSetupListenerBase;
 import org.cobbzilla.wizard.resources.AbstractEntityConfigsResource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import wordland.WordlandApiClient;
 import wordland.model.support.AccountSession;
 import wordland.server.WordlandConfiguration;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import java.util.Set;
+import java.util.TreeSet;
 
 import static lombok.AccessLevel.PROTECTED;
 import static org.cobbzilla.wizard.model.entityconfig.ModelSetup.setupModel;
 import static org.cobbzilla.wizard.resources.ResourceUtil.*;
+import static org.cobbzilla.wizard.util.SpringUtil.listResources;
 import static wordland.ApiConstants.ENTITY_CONFIGS_ENDPOINT;
 import static wordland.ApiConstants.accountPrincipal;
 
@@ -34,7 +39,26 @@ public class EntityConfigsResource extends AbstractEntityConfigsResource {
         return session.hasAccount() && !session.getAccount().isSuspended();
     }
 
-    @POST @Path("/populate/{name:.+}")
+    @GET @Path("/models")
+    public Response getModelNames (@Context HttpContext ctx) {
+
+        final AccountSession session = accountPrincipal(ctx);
+        if (session == null || !session.hasAccount() || !session.getAccount().isAdmin()) return forbidden();
+
+        final Set<String> names = new TreeSet<>();
+        try {
+            final Resource[] rs = listResources("models/**/manifest.json");
+            for (Resource r : rs) {
+                names.add(r.getFile().getParentFile().getName());
+            }
+        } catch (Exception e) {
+            log.error("getModelNames: "+e, e);
+            return serverError();
+        }
+        return ok(names);
+    }
+
+    @POST @Path("/models/{name:.+}")
     public Response populateModel (@Context HttpContext ctx,
                                    @PathParam("name") String modelName) {
 
