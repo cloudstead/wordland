f1a183f518b54aa8fbef2936113dc21d244a05bf
Jonathan Cobb <jonathan@kyuss.org>
2018-10-02 10:00:27 -0400

    set base uri for docs

--- wordland-server/src/main/java/wordland/resources/ApiDocsResource.java
+++ wordland-server/src/main/java/wordland/resources/ApiDocsResource.java
@@ -2,7 +2,9 @@ package wordland.resources;
 
 import lombok.Getter;
 import org.cobbzilla.wizard.resources.OpenApiDocsResource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import wordland.server.WordlandConfiguration;
 
 import javax.ws.rs.Path;
 
@@ -11,6 +13,7 @@ import static lombok.AccessLevel.PROTECTED;
 @Path("/docs") @Service
 public class ApiDocsResource extends OpenApiDocsResource {
 
+    @Getter @Autowired private WordlandConfiguration configuration;
     @Getter(PROTECTED) private final String[] packagesToScan = {"wordland.resources"};
 
 }
